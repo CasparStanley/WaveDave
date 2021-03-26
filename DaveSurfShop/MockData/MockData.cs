6c281fadbbbccde94f2472f33e0d4b84b8007506
Michael(Desktop) <vedar96@gmail.com>
2021-03-26 12:15:48 +0100

    MockData added

--- DaveSurfShop/MockData/MockData.cs
+++ DaveSurfShop/MockData/MockData.cs
@@ -7,6 +7,17 @@ namespace DaveSurfShop.MockData
 {
     public class MockData
     {
-        // Michael was here
+        public static List<LocalEvent> events = new List<LocalEvent>()
+        {
+            new LocalEvent("420, birthday", new DateTime(2021, 4, 20),"Berlin"),
+            new LocalEvent("Summer of 69 party", new DateTime(2021, 6, 4), "The love dungeon"),
+            new LocalEvent("It happens now", DateTime.Now, "Just here"),
+            new LocalEvent("Too late mate", new DateTime(1999, 12, 31), "Y2K beach")
+        };
+
+        public List<LocalEvent> GetEvents()
+        {
+            return events;
+        }
     }
 }
